d43926d513829f4f45c4baed7d1591a6886877a8
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-04-05 02:28:13 +0800

    Create 0175.sql

---
+++ 0175.sql
@@ -0,0 +1,7 @@
+# 175. Combine Two Tables
+# Write your MySQL query statement below
+select p.firstName, p.lastName, a.city, a.state
+from Person as p
+left join Address as a
+    on p.personId = a.personId
+group by p.personId
