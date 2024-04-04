b29614bf5e8099f5178816e762ca5d117d481f80
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-04-04 23:51:03 +0800

    Create 1075.sql

---
+++ 1075.sql
@@ -0,0 +1,8 @@
+# 1075. Project Employees I
+# Write your MySQL query statement below
+select p.project_id, 
+    round(avg(e.experience_years), 2) as average_years
+from Project as p
+join Employee as e
+    on e.employee_id = p.employee_id
+group by p.project_id
