a040c3b9eeadc257eb89e5e75a18ae31825d24d1
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-04-05 01:27:06 +0800

    Create 2356.sql

---
+++ 2356.sql
@@ -0,0 +1,6 @@
+# 2356. Number of Unique Subjects Taught by Each Teacher
+# Write your MySQL query statement below
+select t.teacher_id,
+    count(distinct t.subject_id) as cnt
+from Teacher as t
+group by t.teacher_id
