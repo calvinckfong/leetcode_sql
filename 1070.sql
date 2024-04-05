05202e916df18f7ad1b6731fb3e6b15585cf409a
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-04-05 17:57:01 +0800

    Create 1070.sql

---
+++ 1070.sql
@@ -0,0 +1,14 @@
+# 1070. Product Sales Analysis III
+# Write your MySQL query statement below
+
+select product_id,
+    year as first_year,
+    quantity,
+    price
+from Sales
+    where (product_id, year) in
+    (
+        select product_id, min(year) as year
+        from Sales
+        group by product_id
+    )
