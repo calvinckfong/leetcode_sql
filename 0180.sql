510194a479790282d34d8e881d3f187ba8b68856
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-04-05 23:19:14 +0800

    Create 0180.sql

---
+++ 0180.sql
@@ -0,0 +1,9 @@
+# 180. Consecutive Numbers
+# Write your MySQL query statement below
+select 
+    distinct(l1.num) as ConsecutiveNums
+from logs as l1, logs as l2, logs as l3
+where l1.id + 1 = l2.id 
+    and l2.id + 1 = l3.id
+    and l1.num = l2.num
+    and l2.num = l3.num
