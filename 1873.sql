31330544e7a0b6b4eeed13bf86a0de3be9a3cc76
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-04-07 17:13:07 +0800

    Create 1873.sql

---
+++ 1873.sql
@@ -0,0 +1,6 @@
+# 1873. Calculate Special Bonus
+# Write your MySQL query statement below
+select employee_id, 
+    if (employee_id%2 and name not like 'M%', salary, 0) as bonus
+from Employees
+order by employee_id
