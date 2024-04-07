2e62769407118dfdd3b62bd46a5fa8b05cef8863
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-04-07 16:49:42 +0800

    Create 1587.sql

---
+++ 1587.sql
@@ -0,0 +1,10 @@
+# 1587. Bank Account Summary II
+# Write your MySQL query statement below
+select 
+    u.name,
+    sum(t.amount) as balance
+from Users as u
+left join Transactions as t
+on u.account = t.account
+group by u.account
+having balance > 10000
