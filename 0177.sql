35da8369895026b100038e9781e4480020cf3491
Calvin Fong <calvinckfong@yahoo.com.hk>
2024-04-08 14:23:10 +0800

    Create 0177.sql

---
+++ 0177.sql
@@ -0,0 +1,13 @@
+# 177. Nth Highest Salary
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  SET N = N-1;
+  RETURN (
+      # Write your MySQL query statement below.
+        select distinct salary
+        from Employee
+        order by salary desc
+        limit 1 
+        offset N
+  );
+END
